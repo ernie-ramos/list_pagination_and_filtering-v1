a823f4f476db2aefcdd5951faa64bc2dc5516498
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-05-29 12:12:57 -0400

    Rough draft of project
    Have created showPage and appendPageLinks but calling showPage from inside the former is not working.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# list_pagination_and_filtering-v1
- Unit 2 Project: List Pagination and Filtering
+#Unit 2 Project: List Pagination and Filtering
+
+#ReadMe to-do:
+-A description of the project in your own words
+-The skills, techniques and process used to complete the project
+-If you’ve attempted to earn an “Exceeds Expectations” grade, list the “Extra Credit” features you added.
+
+# PROGRESSIVE ENHANCEMENT
+
+#Core Principles
+The progressive enhancement strategy consists of the following core principles:
+
+Basic content should be accessible to all web browsers
+Basic functionality should be accessible to all web browsers
+Sparse, semantic markup contains all content
+Enhanced layout is provided by externally linked CSS
+Enhanced behavior is provided by unobtrusive, externally linked JavaScript
+End-user web browser preferences are respected
+
+
+#Pseudo for Progressive Enhancement
+if('querySelector' in document
+  && 'localStorage' in window
+  && 'addEventListener' in window) {
+  // bootstrap the javascript application
+}
+
+#avoid disabling zoom
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+#use feature detection to ensure that you’re implementing with progressive enhancement in mind
+if ('serviceWorker' in navigator) {
+  // check to see if service worker API exists
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
--- js/script.js
+++ js/script.js
@@ -2,48 +2,115 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+/**
+ * @const {DOM elemebt} listItem - Stores the student list_item elements in the
+ *                               student list.
+ * @const {num} numItems - Stores the number of items to show on each “page”,
+ *                       which is 10.
+ */
+const listItem = document.querySelectorAll('li');
+const numItems = 10;
+
+/**
+ * The showPage function hides all of the items in the list except for the ten
+ * you want to show.
+ *
+ * @par {list} list - The list parameter to represent the actual list of
+ *                    students that you’ll pass in as an argument later
+ *                    when you call this function.
+ * @par {num} page - The page parameter represents the page number that you’ll
+ *                   pass in as an argument later when you call this function.
+ * @const {num} startIndex - The start index of the list items to be displayed on
+ *                         the given page.
+ * @const {num} endIndex - The end index of the list items to be displayed on
+ *                       the given page.
+ * @const {DOM element} li - <li> elements that have been filtered by index.
+ * @const {DOM element} ul - Parent of li for hiding all and showing desired items.
+ * @if {i} li - Using the index, display any list item with an index that is
+ *              greater than or equal to the start index variable and less than
+ *              the end index variable.
+ */
+function showPage(list, page) {
+  const startIndex = (page * numItems) - numItems;
+  const endIndex = page * numItems;
+
+  for (var i = 0; i < list.length; i++) {
+    const li = list[i];
+    const ul = li.parentNode;
+    ul.removeChild(li)
+
+    if (i >= startIndex && i < endIndex) {
+      ul.appendChild(li);
+    }
+  }
+};
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+
+/**
+ * The appendPageLinks function generates, appends, and adds
+ * functionality to the pagination buttons.
+ *
+ * @par {list} list - The list parameter represents the actual list of
+ *                    items that you’ll pass in as an argument later
+ *                    when you call this function.
+ * @var {num} startIndex - The start index of the list items to be displayed on
+ *                         the given page.
+ * @var {num} endIndex - The end index of the list items to be displayed on
+ *                       the given page.
+ * @var {DOM element} li - <li> elements that have been filtered by index.
+ * @var {DOM element} ul - Parent of li for hiding all and showing desired items.
+ * @if {i} li - Using the index, display any list item with an index that is
+ *              greater than or equal to the start index variable and less than
+ *              the end index variable.
+ */
+function appendPageLinks(list) {
+  const pageDiv = document.querySelector('.page')
+  const pagesNeeded = Math.ceil(list.length / numItems);
+
+  /* credit for this beautiful code: Robert Manolis - Student Success Specialist, Treehouse
+   * Helper funcs */
+  const createEl = el => document.createElement(el);
+  const createAndAppendEl = (parent, child) => {
+    const chil = document.createElement(child);
+    parent.appendChild(chil);
+    return chil
+  }
+
+  const appendToPage = createAndAppendEl(pageDiv, 'div');
+
+  const divsInMainDiv = pageDiv.querySelectorAll('div');
+  const paginationDiv = divsInMainDiv[divsInMainDiv.length - 1];
+  paginationDiv.className = "pagination";
+
+  const appendToDiv = createAndAppendEl(appendToPage, 'ul');
+  for (var i = 0; i < pagesNeeded; i++) {
+    const appendToUl = createAndAppendEl(appendToDiv, 'li');
+    const appendToLi = createAndAppendEl(appendToUl, 'a');
+
+    const aSelector = paginationDiv.querySelectorAll('a');
+    aSelector[i].href = "#";
+    const aActive = paginationDiv.querySelector('a');
+    aActive.className = "active";
+    aSelector[i].textContent = i + 1;
+    aSelector[i].addEventListener('click', (e) => {
+      const aSelector = paginationDiv.querySelectorAll('a');
+      for (var i = 0; i < aSelector.length; i++) {
+        aSelector[i].className = "";
+      }
+      e.target.className = "active";
+      const page = e.target.textContent;
+      console.log(list);
+      showPage(list, pages);
+    });
 
 
+  }
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
+}
+showPage(listItem, 1);
+appendPageLinks(listItem);
 
 
 
