588a2fa22822aad895d2885a947faaa36e6f8283
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-06-02 21:14:42 -0400

    Rewrote funcs as an object
    All the code lives inside an object called app now. Saved old code in scripts.js though.
    
    Need to also clear the page when erasing search bar. Right now the search results are stacking.
    
    Search pagination completed.

--- js/script.js
+++ js/script.js
@@ -1,184 +1,112 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
+const liItems = document.querySelectorAll('.student-item');
+const itemsPerPage = 10;
 
-/**
- * @const {DOM elemebt} listItem - Stores the student list_item elements in the
- *                                 student list.
- * @const {num} numItems - Stores the number of items to show on each “page”,
- *                         which is 10.
- */
-const listItem = document.querySelectorAll('li');
-const numItems = 10;
-
-/* credit for this beautiful code: Robert Manolis - Student Success Specialist, Treehouse */
-const createEl = el => document.createElement(el);
-const createAndAppendEl = (parent, child) => {
-  const chil = document.createElement(child);
-  parent.appendChild(chil);
-  return chil
-}
-
-const clearPage = () => {
-  const ul = document.querySelector('.student-list');
-  const liOnCurrentPage = document.querySelectorAll('.student-list li');
-  // erases all items from current page
-  for (var i = 0; i < liOnCurrentPage.length; i++) {
-    const li = liOnCurrentPage[i];
-    ul.removeChild(li)
-  }
-};
-const appendToPage = (list, startIndex, endIndex) => {
-  // appends new items
-  const ul = document.querySelector('.student-list');
-  for (var i = 0; i < endIndex; i++) {
-    if (i >= startIndex && i < endIndex) {
-      const li = list[i];
-      ul.appendChild(li);
+const app = {
+  clear: () => {
+    const allChildren = document.querySelectorAll('.student-item');
+    for (var i = 0; i < allChildren.length; i++) {
+      const child = document.querySelector('.student-item');
+      const parent = child.parentNode;
+      parent.removeChild(child);
     }
-  }
-};
-/**
- * The showPage function hides all of the items in the list except for the ten
- * you want to show.
- *
- * @par {list} list - The list parameter to represent the actual list of
- *                    students that you’ll pass in as an argument later
- *                    when you call this function.
- * @par {num} page - The page parameter represents the page number that you’ll
- *                   pass in as an argument later when you call this function.
- * @const {num} startIndex - The start index of the list items to be displayed on
- *                           the given page.
- * @const {num} endIndex - The end index of the list items to be displayed on
- *                         the given page.
- * @const {DOM element} li - <li> elements that have been filtered by index.
- * @const {DOM element} ul - Parent of li for hiding all and showing desired items.
- * @if {i} li - Using the index, display any list item with an index that is
- *              greater than or equal to the start index variable and less than
- *              the end index variable.
- */
-function showPage(list, page) {
-  const startIndex = (page * numItems) - numItems;
-  let endIndex = page * numItems;
-  if (endIndex > list.length) {
-    endIndex = list.length;
-  }
-
-  const ul = document.querySelector('.student-list');
-
-  clearPage();
-  appendToPage(list, startIndex, endIndex);
-
-};
-
-/**
- * The appendPageLinks function generates, appends, and adds
- * functionality to the pagination buttons.
- *
- * @par {list} list - The list parameter represents the actual list of
- *                    items that you’ll pass in as an argument later
- *                    when you call this function.
- * @var {num} startIndex - The start index of the list items to be displayed on
- *                         the given page.
- * @var {num} endIndex - The end index of the list items to be displayed on
- *                       the given page.
- * @var {DOM element} li - <li> elements that have been filtered by index.
- * @var {DOM element} ul - Parent of li for hiding all and showing desired items.
- * @if {i} li - Using the index, display any list item with an index that is
- *              greater than or equal to the start index variable and less than
- *              the end index variable.
- */
-function appendPageLinks(list) {
-  const pageDiv = document.querySelector('.page')
-  const pagesNeeded = Math.ceil(list.length / numItems);
-
-
-  const appendToPage = createAndAppendEl(pageDiv, 'div');
-  const divsInMainDiv = pageDiv.querySelectorAll('div');
-  const paginationDiv = divsInMainDiv[divsInMainDiv.length - 1];
-  paginationDiv.className = "pagination";
-
-  const appendToDiv = createAndAppendEl(appendToPage, 'ul');
-  for (var i = 0; i < pagesNeeded; i++) {
-    const appendToUl = createAndAppendEl(appendToDiv, 'li');
-    const appendToLi = createAndAppendEl(appendToUl, 'a');
-
-    const aSelector = paginationDiv.querySelectorAll('a');
-    aSelector[i].href = "#";
-    const aActive = paginationDiv.querySelector('a');
-    aActive.className = "active";
-    aSelector[i].textContent = i + 1;
-    aSelector[i].addEventListener('click', (e) => {
-      const aSelector = paginationDiv.querySelectorAll('a');
-      for (var i = 0; i < aSelector.length; i++) {
-        aSelector[i].className = "";
-      }
-      e.target.className = "active";
-      const page = parseInt(e.target.textContent);
-      showPage(list, page);
-    });
-  }
-}
-
-
-/*
-create a form and a button
-
-as the form is being typed into (at keyUp event) search
-the entire listItem for the content of the form
-
-when the button is pressed search listItem for then
-content of the form
-
-if there's no result say No Results Found
-
-<div class="student-search">
-  <input placeholder="Search for students...">
-  <button>Search</button>
-</div>
-
-*/
-
-const searchComponent = (list) => {
-  showPage(list, 1);
-  appendPageLinks(list);
-  const pagesNeeded = Math.ceil(list.length / numItems);
-  const pageHeader = document.querySelector('.page-header');
-  const searchCompsDiv = createAndAppendEl(pageHeader, 'div');
-  searchCompsDiv.className = "student-search";
-  const searchCompsInput = createAndAppendEl(searchCompsDiv, 'input');
-  searchCompsInput.placeholder = "Search by name...";
-  // const searchCompsButton = createAndAppendEl(searchCompsDiv, 'button');
-  // searchCompsButton.textContent = "Search";
-
-  searchCompsInput.addEventListener('keyup', (e) => {
-    clearPage();
-    const ul = document.querySelector('.student-list');
-    for (var i = 0; i < list.length; i++) {
-      const studentName = list[i].querySelector('.student-details h3').innerText;
-      const searchInput = searchCompsInput.value;
-      if (studentName.includes(searchInput)) {
-        if (document.querySelector('.student-list p')) {
-          ul.removeChild(document.querySelector('.student-list p'));
+  },
+  createElWithAttrProp: (arr) => {
+    const element = document.createElement(arr[0]);
+    element[arr[1]] = arr[2];
+    return element
+  },
+  appendEl: (parent, child) => {
+    parent.appendChild(child);
+  },
+  searchBar: (parent, divProp, divAttr, inpProp, inpAttr) => {
+    const par = document.querySelector(parent);
+    const searchDiv = app.createElWithAttrProp(['div', divProp, divAttr]);
+    app.appendEl(par, searchDiv);
+    const searchInput = app.createElWithAttrProp(['input', inpProp, inpAttr]);
+    app.appendEl(searchDiv, searchInput);
+    return searchInput
+  },
+  searchFunction: () => {
+    const search = app.searchBar('.page-header', 'className', 'student-search', 'placeholder', 'Search by name...' );
+    search.addEventListener('keyup', (e) => {
+      app.clear();
+      for (var i = 0; i < liItems.length; i++) {
+        const studentName = liItems[i].querySelector('.student-details h3').innerText;
+        const searchInput = search.value;
+        if (studentName.includes(searchInput)) {
+          const parent = document.querySelector('.student-list');
+          app.appendEl(parent, liItems[i]);
         }
-        showPage(list, pagesNeeded);
+      }
+      if (document.querySelector('.student-item')) {
+        const par = document.querySelector('.page')
+        const chi = document.querySelector('.pagination')
+        par.removeChild(chi)
+      }
+      const newList = [];
+      const allChildren = document.querySelectorAll('.student-item');
+      for (var i = 0; i < allChildren.length; i++) {
+        newList.push(allChildren[i]);
+      }
+      app.pagination(newList);
+    })
+  },
+  pagination: (list) => {
+    const page = Math.ceil(list.length / itemsPerPage);
+    const par = document.querySelector('.page');
+    const div = app.createElWithAttrProp(['div', 'className', 'pagination']);
+    const ul = document.createElement('ul');
+    app.appendEl(par, div);
+    app.appendEl(div, ul);
+    for (var i = 1; i <= page; i++) {
+      const li = document.createElement('li');
+      const a = app.createElWithAttrProp(['a', 'href', "#"]);
+      a.textContent = i;
+      a.className = '';
+      const liParent = document.querySelector('.pagination ul');
+      app.appendEl(liParent, li);
+      app.appendEl(li, a);
+    }
+    const a = document.querySelectorAll('.pagination ul li a');
+    a[0].className = 'active';
+    let pressed = '';
+    for (var i = 0; i < a.length; i++) {
+      a[i].addEventListener('click', (e) => {
+        app.clear()
+        for (var i = 0; i < a.length; i++) {
+          a[i].className = "";
+        }
+        e.target.className = "active";
+        pressed = document.querySelector('.active').textContent;
+        app.listItems(list, pressed);
+      });
+    }
+  },
+  appendToPage: (list, startIndex, endIndex) => {
+    const ul = document.querySelector('.student-list');
+    for (var i = 0; i < endIndex; i++) {
+      if (i >= startIndex && i < endIndex) {
+        const li = list[i];
+        ul.appendChild(li);
       }
     }
-  })
-  searchCompsInput.addEventListener('keyup', (e) => {
-    const ul = document.querySelector('.student-list');
-    const li = ul.querySelectorAll('.student-item');
-    const pageDiv = document.querySelector('.page');
-    const paginationButtons = document.querySelector('.pagination');
-    const message = document.querySelectorAll('.student-list p');
-    if (li.length == 0 && message.length == 0) {
-      const p = createEl('p');
-      p.innerText = "Uh-oh! Try another name!";
-      ul.appendChild(p);
-      // pageDiv.removeChild(paginationButtons);
+  },
+  listItems: (list, pressed = 1) => {
+    for (var i = 0; i < pressed; i++) {
+      let startIndex = (pressed * itemsPerPage) - itemsPerPage;
+      let endIndex = pressed * itemsPerPage;
+      if (endIndex > list.length) {
+        endIndex = list.length;
+      }
+      app.appendToPage(list, startIndex, endIndex);
     }
-  })
-};
+  },
+  run: () => {
+    app.clear();
+    const search = app.searchFunction();
+    const items = app.listItems(liItems);
+    const pagination = app.pagination(liItems);
 
-searchComponent(listItem);
+  }
+};
+app.run()
